4077e3e0a9b130327b78aeac07d9a480bab83a63
Lisa Chin Mollica <21robots@gmail.com>
2025-09-29 16:30:36 -0400

    Add Apple and Google sign-in packages

--- flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,9 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import google_sign_in_ios
 import path_provider_foundation
+import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
-``
